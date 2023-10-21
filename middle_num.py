bf8d76b15d5c9510771125124231750bd9bfee22
TigranVard <146510780+TigranVard@users.noreply.github.com>
2023-10-21 16:43:20 +1000

    Add files via upload

---
+++ middle_num.py
@@ -0,0 +1,29 @@
+# A = int(input("введите число"))
+# B = int(input("введите число"))
+# C = int(input("введите число"))
+# D = int(input("введите число"))
+# if A = 0
+#     print("Ноль не подходит")
+# if B = 0
+#     print("Ноль не подходит")
+# if C = 0
+#     print("Ноль не подходит")
+
+# mid_num = (A + B + C + D)/4
+# print(round(mid_num,2))
+
+kol = 0 
+
+a = int(input("введите число "))
+kol = kol + 1
+
+sum = 0
+
+while a !=0:
+    print("вы ввели" kol "чисел")
+    sum = sum + a
+    print("сумма чисел равна",sum)
+    print("цикл продолжается")
+    a = int(input("Введите число A "))
+    kol = kol + 1   
+ 
