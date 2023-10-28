70c41578d32206386acbd16eef4093a747eece8b
TigranVard <146510780+TigranVard@users.noreply.github.com>
2023-10-28 15:45:44 +1000

    Add files via upload

--- middle_num.py
+++ middle_num.py
@@ -12,18 +12,25 @@
 # mid_num = (A + B + C + D)/4
 # print(round(mid_num,2))
 
-kol = 0 
+# kol = 0 
 
-a = int(input("введите число "))
-kol = kol + 1
+# a = int(input("введите число "))
+# kol = kol + 1
 
-sum = 0
+# sum = 0
 
-while a !=0:
-    print("вы ввели" kol "чисел")
-    sum = sum + a
-    print("сумма чисел равна",sum)
-    print("цикл продолжается")
-    a = int(input("Введите число A "))
-    kol = kol + 1   
+# while a !=0:
+#     print("вы ввели" kol "чисел")
+#     sum = sum + a
+#     print("сумма чисел равна",sum)
+#     print("цикл продолжается")
+#     a = int(input("Введите число A "))
+#     kol = kol + 1   
  
+letter = input("Введи любую латинскую букву ")
+if letter.lower() in ['a','e','i','o','u']:
+    print("Это гласная буква")
+elif letter.lower() == 'y':
+    print("Это буква может быть гласной и согласной")
+else:
+    print("это согласная буква")
